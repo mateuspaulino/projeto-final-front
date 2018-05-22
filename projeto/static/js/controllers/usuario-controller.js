e7472cda6864f8609bfda9792308d9c82684d92c
Mateus Paulino <contato@mateuspaulino.com.br>
2018-05-22 18:03:33 -0300

    capturando dados incluir usuario

--- projeto/static/js/controllers/usuario-controller.js
+++ projeto/static/js/controllers/usuario-controller.js
@@ -1,3 +1,35 @@
-app.controller("usuarioController", function($scope){
+app.controller("usuarioController", function($scope, $http){
+    $scope.usuario = {};
+    $scope.tipos = {};
+    $http({method:'GET', url:'http://18.228.37.157/reprografiaapi/suporte/perfil/listar'})
+    .then(function (response){
+        $scope.tipos = response.data;
+        // console.log(response.data);
+    } , function (response){
+        // console.log(response);
+    });
+
+    $scope.cadastrar= function(){
+        
+        console.log($scope.usuario);
+        if($scope.usuario.ativo==="true"){
+            $scope.usuario.ativo = true;
+        }else{
+            $scope.usuario.ativo = false;
+        }
+        $scope.usuario.tipoPessoa = "normal";
+        $http({
+            method:'POST', 
+            url:'http://18.228.37.157/reprografiaapi/suporte/pessoa/cadastrar',
+            data: $scope.usuario
+        })
+        .then(function (response){
+            console.log(response);
+        } , function (response){
+            console.log(response);
+        });
+              
+    }
+
 
 })
