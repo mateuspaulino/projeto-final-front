f982d44bac6d36f657910093e2a1904de659df87
Mateus Paulino <mateuspaulino@MacBook-Pro-de-Mateus.local>
2018-06-18 03:28:05 -0300

    tentando alterar

--- projeto/static/js/controllers/editar-requisicao-especifico-controller.js
+++ projeto/static/js/controllers/editar-requisicao-especifico-controller.js
@@ -60,46 +60,94 @@ app.controller("editarRequisicaoEspecificoController", function($scope, $routePa
     };
     dadosUsuario();
 
-    // $scope.tipos = {};
-    // $scope.tipoSelecionado = {};
+    
+    // Pegar relacao professor-disciplina, para depois enviar na requisicao o id
 
-    // $http({method:'GET', url:'http://18.228.37.157/reprografiaapi/suporte/perfil/listar'})
-    // .then(function (response){
-    //     $scope.tipos = response.data;
-    //     // console.log(response.data);
-    // } , function (response){
-    //     // console.log(response);
-    // });
+    $scope.associacaoProfessorDisciplina = {};
+
+    $http({method:'GET', url:'http://18.228.37.157/reprografiaapi/professor/professordisciplina/listarporprofessor'})
+    .then(function (response){
+        var disciplinasProfessores = response.data;
+        $scope.associacaoProfessorDisciplina = response.data;
+        console.log(disciplinasProfessores);
+    } , function (response){
+        // alert("Sessão expirada");
+        // logout();
+    });
 
     $scope.alterar= function(){
         
-        if($scope.usuario.ativo==="true"){
-            $scope.usuario.ativo = true;
-        }else{
-            $scope.usuario.ativo = false;
-        }
-        $scope.usuario.tipoPessoa = "normal";
-        $scope.usuario.perfil.id = parseInt($scope.tipoSelecionado);
+        $scope.requisicao.duplex = $scope.requisicao.duplex==="true"?true:false;
+        $scope.requisicao.colorida = $scope.requisicao.colorida==="true"?true:false;
+        $scope.requisicao.grampeada = $scope.requisicao.grampeada==="true"?true:false;
 
-        delete $scope.usuario.perfil.nome;
-        delete $scope.usuario.perfil.descricao;
-        delete $scope.usuario.salvo;
-        delete $scope.usuario.naoSalvo;
-        console.table($scope.usuario);
+        //pega id professor disciplina
+
+        var filtroProfessorAssociacoes= $scope.associacaoProfessorDisciplina.filter(function( obj ) {
+            return obj.disciplina.id == $scope.disciplinaSelecionada;
+        })
+        // console.log('dos profesor');
+        // console.log(filtroProfessorAssociacoes[0].id);
+
+        $scope.requisicao.professorDisciplina = {id:filtroProfessorAssociacoes[0].id};
+
+        var objAlterar = {
+            "id": $scope.requisicao.id,
+            "duplex":  $scope.requisicao.duplex,
+            "grampeada":  $scope.requisicao.grampeada,
+            "colorida":  $scope.requisicao.colorida,
+            "numeroDePaginas":  $scope.requisicao.numeroDePaginas,
+            "numeroDeCopias":  $scope.requisicao.numeroDeCopias,
+            "professorDisciplina": {
+                "id": filtroProfessorAssociacoes[0].id
+            }
+        };
+
+        var arquivo = $scope.arquivo;
+        console.log('arquivo');
+        console.log(arquivo);
+        var fd = new FormData();
+        fd.append('arquivo-part', arquivo);
+        // "professorDisciplina": {"id":1}
+
+        // fd.append('file',file);
+        // fd.append('meta-part',angular.toJson($scope.requisicao));
+        // fd.append('meta-part',JSON.stringify($scope.requisicao));
+        fd.append('meta-part',JSON.stringify(objAlterar));
+
+        // console.log($scope.requisicao);
+        // console.log(JSON.stringify($scope.requisicao));
+        console.log(objAlterar);
+        console.log(JSON.stringify(objAlterar));
+
+        // $scope.usuario.tipoPessoa = "normal";
+        
 
         $http({
-            method:'POST', 
-            url:'http://18.228.37.157/reprografiaapi/suporte/pessoa/alterar',
-            data: $scope.usuario
-        })
-        .then(function (response){
-            alert("Usuário alterado com sucesso");
-            // $scope.usuario = {};
-            // redirecionar para a lista
-            $location.path("/editar-usuario");
-        } , function(){
-            alert("Sessão expirada");
-            logout();
+            method : 'POST',
+            url : 'http://18.228.37.157/reprografiaapi/professor/requisicao/alterar',
+            headers : {
+                'Content-Type' : undefined
+            },
+            transformRequest : function(data) {
+                var formData = new FormData();
+                formData.append('meta-part', new Blob([ angular.toJson(objAlterar) ], {
+                    type : "application/json"
+                }));
+                formData.append("arquivo-part", arquivo);
+                return formData;
+            },
+            data : {
+                meta_part : objAlterar,
+                imagem_part : arquivo
+            }
+        }).then(function(response) {
+            alert("Requisição alterada com sucesso");
+            $location.path("/editar-requisicao");
+        }, function(response) {
+            alert(response);
+            console.log('erro');
+            console.log(response);
         });
               
     }
