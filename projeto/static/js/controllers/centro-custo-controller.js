d5fd737b3399d2ea545ec14d6bcea9562c894e21
Mateus Paulino <mateuspaulino@MacBook-Pro-de-Mateus.local>
2018-06-18 23:41:06 -0300

    validacao datas custo

--- projeto/static/js/controllers/centro-custo-controller.js
+++ projeto/static/js/controllers/centro-custo-controller.js
@@ -22,8 +22,29 @@ app.controller("centroCustoController", function($scope, $http){
     $scope.cadastrar= function(){
         $scope.custo.dataInicio = $("#dataInicio").val();
         $scope.custo.dataFim = $("#dataFim").val();
-        console.log($scope.custo);
-        console.log(JSON.stringify($scope.custo));
+
+        //Convertendo em novas datas 
+        var dtUmComp = new Date($scope.custo.dataInicio.replace(/(\d{2})\/(\d{2})\/(\d{4})/,'$2/$1/$3')); 
+        var dtDoisComp = new Date($scope.custo.dataFim.replace(/(\d{2})\/(\d{2})\/(\d{4})/,'$2/$1/$3')); 
+        //Exemplo de comparação de datas 
+        if(dtUmComp > dtDoisComp){
+            alert('A data de início não pode ser após a data de término')
+        }else{
+            $http({
+                method:'POST', 
+                url:'http://18.228.37.157/reprografiaapi/gerente/centrodecusto/cadastrar',
+                data: $scope.custo
+            })
+            .then(function (response){
+                alert("custo cadastrado com sucesso");
+                $scope.custo = {};
+                carregarCusto();
+            } , function(){
+                alert("Periodo já existente ou sessão expirada");
+            });
+        }
+
+        // console.log(JSON.stringify($scope.custo));
         // if($scope.custo.ativo==="true"){
         //     $scope.custo.ativo = true;
         // }else{
@@ -31,18 +52,7 @@ app.controller("centroCustoController", function($scope, $http){
         // }
         // $scope.custo.tipoPessoa = "normal";
         // $scope.custo.perfil.id = parseInt( $scope.custo.perfil.id);
-        $http({
-            method:'POST', 
-            url:'http://18.228.37.157/reprografiaapi/gerente/centrodecusto/cadastrar',
-            data: $scope.custo
-        })
-        .then(function (response){
-            alert("custo cadastrado com sucesso");
-            $scope.custo = {};
-            carregarCusto();
-        } , function(){
-            alert("Periodo já existente ou sessão expirada");
-        });
+        
               
     }
 
